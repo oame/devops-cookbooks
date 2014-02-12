7af9b59191615e8091b80a62e2842c7b52b8c96f
Yasuaki Uechi <uetchy@randompaper.co>
2014-02-12 14:08:23 +0900

    Add jenkins

--- jenkins/recipes/default.rb
+++ jenkins/recipes/default.rb
@@ -10,64 +10,46 @@
 include_recipe "java"
 
 # Create the Jenkins user
-user node['jenkins']['master']['user'] do
-  home node['jenkins']['master']['home']
+user node['jenkins']['user'] do
+  home node['jenkins']['home']
 end
 
 # Create the Jenkins group
-group node['jenkins']['master']['group'] do
-  members node['jenkins']['master']['user']
+group node['jenkins']['group'] do
+  members node['jenkins']['user']
 end
 
 # Create the home directory
-directory node['jenkins']['master']['home'] do
-  owner     node['jenkins']['master']['user']
-  group     node['jenkins']['master']['group']
+directory node['jenkins']['home'] do
+  owner     node['jenkins']['user']
+  group     node['jenkins']['group']
   mode      '0755'
   recursive true
 end
 
 # Create the log directory
-directory node['jenkins']['master']['log_directory'] do
-  owner     node['jenkins']['master']['user']
-  group     node['jenkins']['master']['group']
+directory node['jenkins']['log_directory'] do
+  owner     node['jenkins']['user']
+  group     node['jenkins']['group']
   mode      '0755'
   recursive true
 end
 
 case node['platform_family']
-when 'debian'
-  include_recipe 'apt::default'
-
-  apt_repository 'jenkins' do
-    uri          'http://pkg.jenkins-ci.org/debian'
-    distribution 'binary/'
-    key          'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key'
-  end
-
-  package 'jenkins' do
-    version node['jenkins']['master']['version']
-  end
-
-  template '/etc/default/jenkins' do
-    source   'jenkins-config-debian.erb'
-    mode     '0644'
-    notifies :restart, 'service[jenkins]', :immediately
-  end
 when 'rhel'
-  include_recipe 'yum::default'
+  include_recipe "yum::default"
 
   yum_repository 'jenkins-ci' do
-    baseurl 'http://pkg.jenkins-ci.org/redhat'
-    gpgkey  'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
+    url 'http://pkg.jenkins-ci.org/redhat'
+    key 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
   end
 
   package 'jenkins' do
-    version node['jenkins']['master']['version']
+    version node['jenkins']['version']
   end
 
   template '/etc/sysconfig/jenkins' do
-    source   'jenkins-config-rhel.erb'
+    source   'jenkins-config.erb'
     mode     '0644'
     notifies :restart, 'service[jenkins]', :immediately
   end
@@ -78,9 +60,19 @@ service 'jenkins' do
   action  [:enable, :start]
 end
 
+# nginx
 directory "/etc/nginx/sites-available" do
   owner "root"
   group "root"
   mode 0644
-  action :create_if_missing
+  action :create
+end
+
+template '/etc/nginx/sites-available/jenkins' do
+  source   'nginx_jenkins.conf.erb'
+  mode     '0644'
+end
+
+nginx_site 'jenkins' do
+  notifies :restart, 'service[nginx]'
 end
